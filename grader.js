a9814770933051edc6e9b0a53e89adab11ed91a1
Ubuntu <ubuntu@ip-172-31-6-237.us-west-2.compute.internal>
2013-07-23 20:41:37 +0000

    grader - url reading

--- grader.js
+++ grader.js
@@ -5,6 +5,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "https://www.google.com";
 var rest = require('restler');//added
 var sys = require('util');//added
 
@@ -36,6 +37,22 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var getURL = function(url) {
+    rest.get(program.url).on('complete', function(result) {
+//	console.log(result);
+//	console.log("page read");
+	if(result instanceof Error) {
+            sys.puts('Error: '+ result.message);
+            this.retry(5000);
+	} else {
+//	    sys.puts(result);
+	console.log("page read 2" + url);
+	}
+    }
+
+)};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.come/a/6772648
@@ -46,12 +63,17 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Command Line defined url') //added
+        .option('-u, --url <url>', 'Command Line defined url', clone(getURL),URL_DEFAULT)
         .parse(process.argv);
+if (!program.file) {
+//    console.log(program.url);
+      console.log("page really read");
+} else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
+} 
+}else {
     exports.checkHtmlFile = checkHtmlFile;
 }
     
